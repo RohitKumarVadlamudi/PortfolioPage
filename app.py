aa9cd95918d0b8ce349464125924ad8a97401b42
RohitKumarVadlamudi <98195539+RohitKumarVadlamudi@users.noreply.github.com>
2025-04-30 00:20:30 -0400

    Replacing main with subbranch

---
+++ app.py
@@ -0,0 +1,19 @@
+/* app.py */
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/social")
+def social():
+    return render_template("social.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
